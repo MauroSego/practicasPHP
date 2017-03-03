a77ecff19e2b7f615be15924dd9c1d05f53bd051
MauroSego <mauro.segovia.7@gmail.com>
2017-03-03 12:14:47 -0300

    Resolucion de ejercicio de echos con divs y css

---
+++ ejercicio1/ejercicioDivs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Ejercicio de creación de divs</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  <?php
+
+  echo "<h1>Aprender PHP</h1>";
+  echo "<h2>Aprendiendo php desde 0</h2>";
+  echo "<div class='amarillo'>Curso php de aprenderaprogramar.com usando la instrucción echo</div>";
+  echo "<div class='gris'>Este es el pie de página</div>"
+
+
+  ?>
+</body>
+</html>
