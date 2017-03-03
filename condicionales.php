39576e9c5788d8296fb3c04af643842595a7a675
MauroSego <mauro.segovia.7@gmail.com>
2017-03-03 12:24:06 -0300

    Condicionales

---
+++ condicionales.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Condicionales</title>
+</head>
+<body>
+  <?php
+    $galletas = false;
+    if($galletas){
+      echo 'Hay galletas';
+    } else {
+      echo "No hay galletas";
+    };
+    
+
+
+  ?>
+</body>
+</html>
