a2c30b695b7737cdb696fd9b65bc1a551e65a159
MauroSego <mauro.segovia.7@gmail.com>
2017-03-06 17:49:16 -0300

    Arrays asociativo y bidimencional

---
+++ arrays/arrayAsociativo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Array Asociativo</title>
+</head>
+<body>
+  <?php
+    $indice = 'coche';
+
+    $colores_vehiculos = array(
+        'coche' => 'Rojo',
+        'moto'  => 'Verde',
+        'avion' => 'Amarillo'
+      );
+    echo $colores_vehiculos[$indice];
+
+  ?>
+</body>
+</html>
---
+++ arrays/arrayBidimencional.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Array Bidimensional</title>
+</head>
+<body>
+  <?php
+
+    $animal[0][0] = 'Perro';
+    $animal[0][1] = 'Gato';
+    $animal[1][0] = 'Lombriz';
+    $animal[1][1] = 'Burro';
+    $animal[2][0] = 'Murciélago';
+    $animal[2][1] = 'Cocodrilo';
+
+    echo $animal[2][1] ;
+    echo $animal[0][0] ;
+
+
+
+  ?>
+</body>
+</html>
